67bc5604247c75494b02ca30dce54ada041c2986
lukbru <bruzdalukasz1c@gmail.com>
2024-09-16 16:31:10 +0200

    Fix: exitHandler

--- backend/src/scrapper.js
+++ backend/src/scrapper.js
@@ -61,6 +61,12 @@ async function startScrapper() {
     }
 }
 
+function exitHandler(options, exitCode) {
+    if (options.cleanup) console.log('clean');
+    if (exitCode || exitCode === 0) console.log(exitCode);
+    if (options.exit) process.exit();
+}
+
 process.on('exit', exitHandler.bind(null,{cleanup:true}));
 process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
