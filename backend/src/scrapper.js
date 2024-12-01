7939b01070110c9a3b407e9b7303d5fb52c4c19d
lukbru <bruzdalukasz1c@gmail.com>
2024-12-01 18:48:06 +0100

    Fix

--- backend/src/scrapper.js
+++ backend/src/scrapper.js
@@ -107,7 +107,7 @@ process.on('SIGINT', exitHandler.bind(null, {exit:true}));
 process.on('SIGUSR1', exitHandler.bind(null, {exit:true}));
 process.on('uncaughtException', exitHandler.bind(null, {exit:true}));
 
-cron.schedule('45 18 * * *', async () => {
+cron.schedule('10 12 * * *', async () => {
     startScrapper();
 });
 
